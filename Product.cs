6f36e58d960d1a7518e9f74478bce1da7542e138
keith <keithjustinemeterio5@gmail.com>
2025-02-26 17:35:42 +0800

    Added product class file

---
+++ Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryDiscountApp
+{
+    internal class Product
+    {
+        private string name;
+        private double price;
+        private int quantity;
+
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+    }
+
+}
